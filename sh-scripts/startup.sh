e6e74519d02f43cc4734baf4292faa27852bd880
myh2910 <minleeyohan@gmail.com>
2022-04-30 02:34:14 -0500

    Use ibus for foreign language support and delete other hostnames

--- sh-scripts/startup.sh
+++ sh-scripts/startup.sh
@@ -9,76 +9,13 @@
 #
 # dropbox no longer, started manually
 
-WS1="1: Aleph"
-WS2="2: Bet"
-WS3="3: Gimel"
-WS4="4: Dalet"
-WS5="5: Hei"
-WS6="6: Vav"
-WS7="7: Zayin"
-WS8="8: Het"
-WS9="9: Tet"
-WS0="10: Yod"
-
 xss-lock -n ~/dotfiles/sh-scripts/lock-warning.sh -- ~/dotfiles/sh-scripts/fuzzy-lock.sh &
 
-if [ "$(hostname)" = ArchAir ]; then
-	dunst &
-	synclient RightButtonAreaRight=1
-	synclient VertScrollDelta=-237
-	systemctl start --user evil-chin.service
-fi
-
-if [ "$(hostname)" = ArchAngel ]; then
-	picom -C -G -b --no-fading-openclose
-	redshift-gtk &
-	dropbox-cli start
-fi
-
-if [ "$(hostname)" = ArchScythe ]; then
-	picom -C -G -b --no-fading-openclose
-	redshift-gtk &
-	dropbox-cli start
-	systemctl --user start evansync.timer # idfk why systemctl enable doesn't work w/e
-	dunst &
-	syncthing-gtk -m &
-	source ~/dotfiles/conky/setup.sh &
-fi
-
-if [ "$(hostname)" = ArchSapphire ]; then
-	picom -C -G -b --no-fading-openclose
-	dunst &
-	source ~/dotfiles/conky/setup.sh &
-	syncthing-gtk -m &
-fi
-
-if [ "$(hostname)" = ArchDiamond ]; then
+if [ "$(hostname)" = myh2910 ]; then
 	picom -C -G -b --no-fading-openclose
 	redshift-gtk &
 	systemctl --user start evansync.timer
 	dunst &
-	syncthing-gtk -m &
-	if [ "$(whoami)" = "evan" ]; then
-		source ~/dotfiles/conky/setup.sh &
-		ibus-daemon -d -r &
-	fi
-	#if [ "$(date +'%Z')" = "PDT" -a "$(whoami)" = "evan" ]; then
-	#	/home/evan/dotfiles/py-scripts/ctwenty.py &
-	#fi
-fi
-
-if [ "$(hostname)" = ArchMajestic ]; then
-	picom -C -G -b --no-fading-openclose
-	if [ "$(whoami)" = "evan" ]; then
-		source ~/dotfiles/conky/setup.sh &
-		#/home/evan/dotfiles/py-scripts/ctwenty.py &
-		ibus-daemon -d -r &
-		dropbox-cli start
-	fi
-	if [ "$(whoami)" = "star" ]; then
-		conky -c ~/dotfiles/conky/star-bar.conf
-	fi
-	redshift-gtk &
-	syncthing-gtk &
-	systemctl --user start evansync.timer
+	source ~/dotfiles/conky/setup.sh &
+	ibus-daemon -d -r &
 fi
